5f70628e7797b384168fed6519801f9f3b641cc4
leena0203 <leenasarode02@gmail.com>
2020-12-17 00:58:29 +0530

    Added: validation to add user name

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends React.Component{
   constructor(){
     super()
     this.state={
-      userName: ''
-      
-    }
+      userName: '',
+      nameError: ''
+      }
   }
 
   //onclick event
@@ -19,7 +19,13 @@ class App extends React.Component{
   }
   onNameChange = (event) => {
     console.log("value is ", event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2, }$');
     this.setState({userName: event.target.value})
+    if (nameRegex.test(event.target.value)){
+      this.setState({ nameError: ''})
+    }else{
+      this.setState({ nameError: 'Name is Incorrect'})
+    }
 
   }
   render(){
@@ -32,6 +38,7 @@ class App extends React.Component{
       </div>
       <div className= "text-box">
         <input onChange={this.onNameChange} />
+        <span className = "error-output">{this.state.nameError}</span>
       </div>
       </>
     );
