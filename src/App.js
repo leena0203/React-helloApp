22552df6a09ce78d833c7d0b599bc55654b07777
leena0203 <leenasarode02@gmail.com>
2020-12-16 17:00:16 +0530

    Added: Logo using Property Binding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/BridgeLabz New Logo.svg';
 
 class App extends React.Component{
   constructor(){
     super()
     this.state={
-      body: 'Hello from capgemini'
+      body: 'Hello All to BridgeLabz'
     }
   }
   render(){
     return(
       <div>
         <h1>{this.state.body}</h1>
+        <img src ={logo} alt ="Logo" />
       </div>
     );
   }
