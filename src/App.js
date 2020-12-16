90aaa54219fb67939397ca80bcb864e9d0f0fe85
leena0203 <leenasarode02@gmail.com>
2020-12-16 16:33:58 +0530

    Added: Constructor to avoid hard core value and introduce one way data binding

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      body: 'Hello from capgemini'
+    }
+  }
   render(){
     return(
       <div>
-        <h1>Hello All!</h1>
+        <h1>{this.state.body}</h1>
       </div>
     );
   }
