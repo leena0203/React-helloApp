cb676d356f5ca644242c6044a50086aad5bf2573
leena0203 <leenasarode02@gmail.com>
2020-12-16 17:18:15 +0530

    Added: Launch Bridgelabz site on click using event binding

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import logo from './assets/BridgeLabz New Logo.svg';
 
 class App extends React.Component{
+  url = 'https://www.bridgelabz.com'
   constructor(){
     super()
     this.state={
       body: 'Hello All to BridgeLabz'
     }
   }
+
+  //onclick event
+  onClick = ($event) => {
+    console.log("save button is clicked!" , $event);
+    window.open(this.url, "_blank");
+  }
   render(){
     return(
       <div>
         <h1>{this.state.body}</h1>
-        <img src ={logo} alt ="Logo" />
+        <img src ={logo} onClick={this.onClick}
+        alt ="Logo" />
       </div>
     );
   }
